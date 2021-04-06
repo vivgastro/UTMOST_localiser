2f118beb02476cc3bf86c1c7b223f9a8adb4f0c4
Vivek Gupta <vgupta@mpsr-srv0.obs.molonglo.local>
2021-04-06 16:15:55 +1000

    Added telescope parameters.py

---
+++ telescope_parameters.py
@@ -0,0 +1,16 @@
+#According to the Doc 'Coordinate transformations for MOST':
+lat=-0.6173567480991081 #Latitude.
+skew=2.3755870374367263e-5 #Skew.
+slope=3.448285788259685e-3 #Slope.
+
+#According to John Reynold's code:
+#lat=-0.617335295908
+#skew=2.37558703744e-05
+#slope=3.44946533287e-3 
+
+#In order to make my definitions consistent with the PDF:
+skew=-skew
+slope=-slope
+
+#But the signs of the angles in the Doc may be wrong (and maybe the values as well)!
+
